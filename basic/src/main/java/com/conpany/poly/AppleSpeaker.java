03822ca59170ad41d13571d96512efd82cf6c055
HyeonJu <guswn8557@gmail.com>
2020-12-31 07:47:05 +0900

    spring updated

---
+++ basic/src/main/java/com/conpany/poly/AppleSpeaker.java
@@ -0,0 +1,19 @@
+package com.conpany.poly;
+
+public class AppleSpeaker implements Speaker {
+
+	public AppleSpeaker() {
+		System.out.println("==== AppleSpeaker 객체 생성");
+	}
+	
+	@Override
+	public void volumeUp() {
+		System.out.println("==== AppleSpeaker Volume Up");
+	}
+	
+	@Override
+	public void volumeDown() {
+		System.out.println("==== AppleSpeaker Volume Down");
+	}
+
+}
---
+++ basic/src/main/java/com/conpany/poly/Speaker.java
@@ -0,0 +1,9 @@
+package com.conpany.poly;
+
+public interface Speaker {
+
+	void volumeUp();
+
+	void volumeDown();
+
+}
